23d71fedff19f24f7cbb22093a997a3c8ab2daa1
warren rhodes <warren.rhodes@spreeloop.com>
2022-04-09 22:02:16 +0100

    add the page route

---
+++ lib/presentation/page/floor_plan/floor_plan_inspiration_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FloorPlanInspirationView extends StatelessWidget {
+  const FloorPlanInspirationView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
