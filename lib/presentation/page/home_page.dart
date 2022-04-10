5dcc602026a008beac9bd67c670bd4733c7a6997
warren rhodes <warren.rhodes@spreeloop.com>
2022-04-10 08:16:11 +0100

    add linter

--- lib/presentation/page/home_page.dart
+++ lib/presentation/page/home_page.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
+/// Display the home page.
 class HomePage extends StatelessWidget {
+  /// Constructor of [HomePage].
   const HomePage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
+  Widget build(BuildContext context) => Container();
 }
